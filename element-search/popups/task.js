ea3433d6de9c5413d3bf7a33389042d2f7711640
knosal <jiupuk2015@yandex.ru>
2022-07-02 13:06:30 +0300

    task №1

---
+++ element-search/popups/task.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const modalMain = document.querySelector('#modal_main');
+const closeModal = document.querySelectorAll('.modal__close_times');
+const showModal = document.querySelector('.show-success');
+const showModalActive = document.querySelector('#modal_success');
+
+
+const closeWindow = () => {
+  for(let i = 0; i < closeModal.length; i++) {
+    closeModal[i].onclick = () => {
+      modalMain.classList.remove('modal_active');
+      showModalActive.classList.remove('modal_active')
+    }
+  }
+}
+closeWindow()
+
+modalMain.classList.add('modal_active');
+
+showModal.onclick = () => { 
+   modalMain.classList.remove('modal_active');
+   showModalActive.classList.add('modal_active');
+}
