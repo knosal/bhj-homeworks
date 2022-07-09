ba899007313d15dc495574ce8ae74009ca6087f4
knosal <jiupuk2015@yandex.ru>
2022-07-09 14:33:20 +0300

    task №1 update

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,16 +1,20 @@
-const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
-const tooltip = document.querySelector('.tooltip');
 
-hasTooltip.forEach(item => item.addEventListener('click', (e) => {
-  e.preventDefault();
-  const { top, left } = item.getBoundingClientRect() 
-  
-  if(tooltip.textContent === e.currentTarget.title) { 
-    tooltip.classList.toggle('tooltip_active');
-  } else {
-    tooltip.textContent = e.currentTarget.title;
-    tooltip.style.top = (top + 20 + 'px');
-    tooltip.style.left = (left + 'px');
-    tooltip.classList.add('tooltip_active');
-  }
-}));
+document.querySelectorAll('.has-tooltip').forEach(item => {
+    const tooltip = document.createElement('div');
+    tooltip.className = 'tooltip';
+    item.after(tooltip);
+
+    item.addEventListener('click', (event) => {
+ 
+    const { top, left } = item.getBoundingClientRect() 
+    if (tooltip.textContent === event.currentTarget.title) { 
+        tooltip.classList.toggle('tooltip_active');
+    } else {
+        tooltip.textContent = event.currentTarget.title;
+        tooltip.setAttribute('data-position', 'bottom');
+        tooltip.setAttribute('style', `left: ${left}px; top: ${top + 20}px;`)
+        tooltip.classList.add('tooltip_active');
+    }
+    event.preventDefault();
+    })
+});
