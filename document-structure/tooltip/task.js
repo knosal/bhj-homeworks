b2c1fc7b5151694883d282cc1b552cf5421a2ab8
knosal <jiupuk2015@yandex.ru>
2022-07-09 13:52:25 +0300

    task №1

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,16 @@
+const hasTooltip = Array.from(document.querySelectorAll('.has-tooltip'));
+const tooltip = document.querySelector('.tooltip');
+
+hasTooltip.forEach(item => item.addEventListener('click', (e) => {
+  e.preventDefault();
+  const { top, left } = item.getBoundingClientRect() 
+  
+  if(tooltip.textContent === e.currentTarget.title) { 
+    tooltip.classList.toggle('tooltip_active');
+  } else {
+    tooltip.textContent = e.currentTarget.title;
+    tooltip.style.top = (top + 20 + 'px');
+    tooltip.style.left = (left + 'px');
+    tooltip.classList.add('tooltip_active');
+  }
+}));
